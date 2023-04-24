d104bd4cd1aa462d2966aff0c154254d3d4ac50a
Daniel WOAGOU <pwoagou@otr.tg>
2023-04-24 18:09:33 +0000

    integration of resultsWrappers

---
+++ Application/Contracts/Services/Account/IAccountService.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Otr.Dccfe.Application.Interfaces.Common;
+using Otr.Dccfe.Application.Requests.Identity;
+using Otr.Dccfe.Shared.Wrapper;
+
+namespace Otr.Dccfe.Application.Interfaces.Services.Account
+{
+    public interface IAccountService : IService
+    {
+        Task<IResult> UpdateProfileAsync(UpdateProfileRequest model, string userId);
+
+        Task<IResult> ChangePasswordAsync(ChangePasswordRequest model, string userId);
+
+        Task<IResult<string>> GetProfilePictureAsync(string userId);
+
+        Task<IResult<string>> UpdateProfilePictureAsync(UpdateProfilePictureRequest request, string userId);
+    }
+}
