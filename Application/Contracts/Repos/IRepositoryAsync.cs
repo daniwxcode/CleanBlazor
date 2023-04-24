cc36c48166c4a202051781c4edc3c9c3f16ca006
Daniel WOAGOU <pwoagou@otr.tg>
2023-04-24 17:49:50 +0000

    Integration of UnitOfWork

---
+++ Application/Contracts/Repos/IRepositoryAsync.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Contracts;
+
+namespace Application.Contracts.Repos
+{
+    public interface IRepositoryAsync<T, in TId> where T : class, IEntity<TId>
+    {
+        IQueryable<T> Entities { get; }
+
+        Task<T> GetByIdAsync(TId id);
+
+        Task<List<T>> GetAllAsync();
+        Task<List<T>> GetPagedResponseAsync(int pageNumber, int pageSize);
+
+        Task<T> AddAsync(T entity);
+        Task<List<T>> AddRangeAsync(List<T> entity);
+
+        Task UpdateAsync(T entity);
+        Task UpdateRangeAsync(List<T> entity);
+
+        Task DeleteAsync(T entity);
+        Task DeleteRangeAsync(List<T> entity);
+    }
+}
---
+++ Application/Contracts/Repos/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Contracts;
+
+namespace Application.Contracts.Repos
+{
+    public interface IUnitOfWork<TId> : IDisposable
+    {
+        IRepositoryAsync<T, TId> Repository<T>() where T : AuditableEntity<TId>;
+        Task<int> Commit(CancellationToken cancellationToken);
+        Task<int> CommitAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys);
+        Task<int> RemoveCache(CancellationToken cancellationToken);
+        Task Rollback();
+    }
+}
