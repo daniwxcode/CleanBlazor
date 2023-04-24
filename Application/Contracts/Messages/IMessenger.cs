b280786835d84d3eee771d0ab1d0c577a4a1dc67
Daniel WOAGOU <pwoagou@otr.tg>
2023-04-24 17:40:58 +0000

    messages contracts

---
+++ Application/Contracts/Messages/IMessenger.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Application.Contracts.Messages;
+
+public interface IMessenger
+{
+    [Column(TypeName = "varchar(50)")]
+    public string FirstName { get; set; }
+
+    [Column(TypeName = "varchar(100)")]
+    public string LastName { get; set; }
+
+    [Column(TypeName = "text")]
+    public string ProfilePictureDataUrl { get; set; }
+}
